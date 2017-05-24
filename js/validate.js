131e3c41ec7f433d1b48dd1e5f71ce2be55955a6
Nate Hascup <nshascup@gmail.com>
2017-05-23 21:11:26 -0500

    initial commital

--- js/validate.js
+++ js/validate.js
@@ -1,1 +1,14 @@
 // JavaScript for form validation goes here!
+
+//Hook up the form submit button to a validate function
+$(document).ready(function(){
+    $('input[type="submit"]').on("click", validate);
+});
+
+//perform validation logic on form inputs
+function validate(event){
+    
+    //prevent 
+}
+
+//
